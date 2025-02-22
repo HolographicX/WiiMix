0b9e87400618f0ffe35652f1ffa737d629213aa7
HolographicX <73580740+HolographicX@users.noreply.github.com>
2025-02-21 21:52:41 -0500

    Update WebAPI.h

--- Source/Core/DolphinQt/WiiMix/WebAPI.h
+++ Source/Core/DolphinQt/WiiMix/WebAPI.h
@@ -7,9 +7,9 @@
 class WiiMixWebAPI {
   public:
     // static void RALoginCallback(std::string username, std::string token); // currently unused
-    static std::map<uint16_t, std::string> getGameAchievements(uint16_t game_id);
-    static std::map<std::string, uint16_t> getGameList(uint16_t console_id); // 16 for gamecube
-    static uint16_t getGameID(uint32_t achievement_id);
+    static std::map<int, std::string> getGameAchievements(int game_id);
+    static std::map<std::string, int> getGameList(int console_id); // 16 for gamecube
+    static int getGameID(int achievement_id);
   private:
     static std::string m_username;
     static std::string m_token;
